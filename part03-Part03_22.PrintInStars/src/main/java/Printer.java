1735da431f8a22508edc936a221e4fda64525785
Raquel Betancourt <114664493+edmblue@users.noreply.github.com>
2024-09-11 09:32:12 -0300

    Add files via upload

---
+++ part03-Part03_22.PrintInStars/src/main/java/Printer.java
@@ -0,0 +1,23 @@
+
+public class Printer {
+
+    public static void main(String[] args) {
+        
+        int[] array = {5, 1, 3, 4, 2};
+        printArrayInStars(array);
+    }
+
+    public static void printArrayInStars(int[] array) {
+        for (int num: array) {
+            printStars(num);
+        }
+    }
+
+    public static void printStars(int number) {
+        for (int i = 0; i < number; i++) {
+            System.out.print("*");
+        }
+        System.out.println("");
+    }
+
+}
